8a761d32f60d91586d526ab0c8b368cf87b66472
ladyfree <83199278+ladyfree@users.noreply.github.com>
2025-02-24 17:44:00 +0900

    Add files via upload

---
+++ python_basic/03_num_operation1.py
@@ -0,0 +1,14 @@
+# 숫자의 산술 연산자를 테스트 해본다.
+
+x = 100
+y = 3
+
+print('덧셈 결과', x+y)
+print('뺄셈 결과', x-y)
+print('곱셈 결과', x*y)
+print('나눗셈 결과', x/y)
+print('내림한 몫 결과', x//y )
+print('나머지 결과', x % y )
+print('x 의 음수곱 결과',-x )
+print('x 의 양수곱 결과',+x )
+print('x 의 y거듭제곱 결과', x**y)
---
+++ python_basic/03_string_multiline.py
@@ -0,0 +1,9 @@
+text = '''hello world
+freinds'''
+
+print(text)
+
+text = """hello world2
+guys"""
+
+print(text)
